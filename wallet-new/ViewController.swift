2cefdfaae2f0630bfd17b17c0034a1ae7ff55d8a
Matis Luzi <matisluzi@gmail.com>
2019-09-18 19:46:18 +0200

    Initial Commit

---
+++ wallet-new/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wallet-new
+//
+//  Created by Matis Luzi on 9/18/19.
+//  Copyright © 2019 Matis Luzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
